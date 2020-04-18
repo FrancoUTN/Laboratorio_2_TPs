52c1d2e241c507b6f80c34ef2c9ad00ff80bbfde
FrancoUTN <cataniafrancoar@gmail.com>
2020-04-18 18:23:33 -0300

    primeraParte

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+	public class Calculadora
+	{
+		public static double Operar(Numero num1, Numero num2, string operador)
+		{
+			return 0;
+		}
+
+		static string ValidarOperador(string operador)
+		{
+			return "";
+		}
+	}
+}
---
+++ TP1/Entidades/Numero.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+	public class Numero
+	{
+		double numero;
+
+		string SetNumero
+		{
+			set
+			{
+				double num = Numero.ValidarNumero(value);
+
+				if (num != 0) // Querrán que, si no, quede en Null?
+					this.numero = num;
+			}
+		}
+
+		public Numero()
+		{
+			this.numero = 0;
+		}
+
+		public Numero(double numero)
+		{
+			this.SetNumero = numero.ToString();
+		}
+
+		public Numero(string strNumero)
+		{
+			this.SetNumero = strNumero;
+		}
+
+		private static double ValidarNumero(string strNumero)
+		{
+			double.TryParse(strNumero, out double number);
+
+			return number;
+		}
+
+
+		public static string BinarioDecimal(string binario)
+		{
+			if (true)
+				return "";
+			else
+				return "Valor inválido";
+		}
+
+		public static string DecimalBinario(double numero)
+		{
+			string bin = "";
+
+			int entero = (int) Math.Abs(numero);
+
+			for (int i = 0; entero >= 2; i++)
+			{
+				bin = (entero % 2).ToString() + bin;
+
+				entero /= 2;
+			}
+
+			bin = entero + bin;
+
+			return bin;
+		}
+
+		public static string DecimalBinario(string numero)
+		{
+			if (double.TryParse(numero, out double num))
+				return Numero.DecimalBinario(num);
+			else
+				return "Valor inválido";
+		}
+	}
+}
